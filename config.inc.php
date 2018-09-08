c66da157141c2f24f8592ed8c5f44868e878cf60
DACRepair <DACRepair@gmail.com>
2018-09-08 18:00:00 -0400

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+###############################################
+# Site configuration
+###############################################
+    $db_host="db";
+    $db_user="user";
+    $db_pass="pass";
+    $db_name="database";
+    $table_prefix="ogp_";
+    $db_type="mysql";
+?>
